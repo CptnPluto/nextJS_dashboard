9d63de0a7c8af1e48e0b38a55431dae4c77af46a
CptnPluto <cptnpluto@gmail.com>
2025-01-27 12:33:52 +0200

    Chapter 9 - streaming data and Suspense.

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,9 @@
-export default function Invoices() {
+export default async function Invoices() {
+	const timeout = await new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("done");
+		}, 400);
+	});
+
 	return <div>Invoices</div>;
 }
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -2,18 +2,13 @@ import { ArrowPathIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Image from "next/image";
 import { lusitana } from "@/app/ui/fonts";
-import { LatestInvoice } from "@/app/lib/definitions";
 import { formatDistanceToNow } from "date-fns";
+import { fetchLatestInvoices } from "@/app/lib/data";
 
-export default function LatestInvoices({
-    latestInvoices,
-    timeAtFetch,
-}: {
-    latestInvoices: LatestInvoice[];
-    timeAtFetch: Date;
-}) {
+export default async function LatestInvoices() {
+    const { latestInvoices, timeAtFetch } = await fetchLatestInvoices();
     const updatedAt = formatDistanceToNow(timeAtFetch, { addSuffix: true });
-
+    
 	return (
 		<div className="flex w-full flex-col md:col-span-4">
 			<h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>Latest Invoices</h2>
