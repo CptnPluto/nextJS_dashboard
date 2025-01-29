d7a8e806d5dcf977e36dc8cfd714f388e59bc6e4
CptnPluto <cptnpluto@gmail.com>
2025-01-29 15:10:19 +0200

    Shifted Customers page to use same Suspense fallback structure as Invoices. Vercel deployment was failing due to weird formatting issues, useSearchParams() not being wrapped in Suspense, etc. Should be fixed now?.

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,6 +1,9 @@
-import { fetchFilteredCustomers } from "@/app/lib/data";
-import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
+import { Suspense } from "react";
+import CustomersTable from "@/app/ui/customers/table";
+import Search from "@/app/ui/search";
+import { CustomersTableSkeleton } from "@/app/ui/skeletons";
+import { lusitana } from "@/app/ui/fonts";
 
 export const metadata: Metadata = {
 	title: "Customers",
@@ -13,7 +16,19 @@ export default async function Page({
 }) {
 	const params = await searchParams;
 	const query = params?.query || "";
-	const customers = await fetchFilteredCustomers(query);
 
-	return <CustomersTable customers={customers} />;
+	return (
+		<div className="w-full">
+			<div className="flex w-full items-center justify-between">
+				<h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+			</div>
+			<div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+				<Search placeholder="Search invoices..." />
+			</div>
+			<Suspense fallback={<CustomersTableSkeleton />}>
+				<CustomersTable query={query} />
+			</Suspense>
+		</div>
+	);
 }
+
