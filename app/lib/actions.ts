c154a1c750fe4b3b1ad68eb6d04113e0ecf18757
CptnPluto <cptnpluto@gmail.com>
2025-01-30 18:27:55 +0200

    Initial insertion into db successful.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,7 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { signIn } from "@/auth";
 import { AuthError } from "next-auth";
+import { User } from "./definitions";
 
 const FormSchema = z.object({
 	id: z.string(),
@@ -20,6 +21,13 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const EditInvoice = FormSchema.omit({ date: true });
 const DeleteInvoice = FormSchema.omit({ customerId: true, amount: true, status: true, date: true });
+const UserSignup = z
+	.object({
+		name: z.string().min(2, { message: "Name must be 2 or more characters long." }),
+		email: z.string().email({ message: "Invalid email address." }),
+		password: z.string().min(6, { message: "Password must be 5 or more characters long." }),
+	})
+	.required();
 
 export type State = {
 	errors?: {
@@ -124,5 +132,35 @@ export async function authenticate(prevState: string | undefined, formData: Form
 			}
 		}
 		throw error;
+	}
+}
+
+export async function signup(previousState: string | undefined, formData: FormData) {
+	// validate formData with zod
+	// check for dublicate user
+	// check for matching password
+	const validatedFields = UserSignup.safeParse({
+		name: formData.get("name"),
+		email: formData.get("email"),
+		password: formData.get("password"),
+	});
+
+	if (!validatedFields.success) {
+		return {
+			errors: validatedFields.error.flatten().fieldErrors,
+			message: "Missing fields. Failed to create invoice.",
+		};
+	}
+
+	const { name, email, password } = validatedFields.data;
+
+	try {
+		await sql`
+        INSERT INTO users (name, email, password)
+        VALUES (${name}, ${email}, ${password})
+    `;
+		// await signIn("credentials", {email: email, password: {password}})
+	} catch (error: any) {
+		return { message: "Failed to insert data into db: ", error: error };
 	}
 }
--- app/ui/signup-form.tsx
+++ app/ui/signup-form.tsx
@@ -6,13 +6,12 @@ import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "./button";
 import { useActionState } from "react";
 import { useSearchParams } from "next/navigation";
-import { authenticate } from "../lib/actions";
-import Link from "next/link";
+import { signup } from "../lib/actions";
 
 export default function SignupForm() {
 	const searchParams = useSearchParams();
 	const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
-	const [errorMessage, formAction, isPending] = useActionState(authenticate, undefined);
+	const [errorMessage, formAction, isPending] = useActionState(signup, null);
 
 	return (
 		<form action={formAction} className="space-y-3">
@@ -20,6 +19,25 @@ export default function SignupForm() {
 				<h1 className={`${lusitana.className} mb-3 text-2xl`}>
 					Please sign up to continue.
 				</h1>
+				<div>
+					<label
+						className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+						htmlFor="name"
+					>
+						Name
+					</label>
+					<div className="relative">
+						<input
+							className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+							id="name"
+							type="name"
+							name="name"
+							placeholder="Enter your name"
+							required
+						/>
+						<AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+					</div>
+				</div>
 				<div className="w-full">
 					<div>
 						<label
@@ -35,26 +53,6 @@ export default function SignupForm() {
 								type="email"
 								name="email"
 								placeholder="Enter your email address"
-								required
-							/>
-							<AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-						</div>
-					</div>
-				<div className="w-full">
-					<div>
-						<label
-							className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-							htmlFor="name"
-						>
-							Name
-						</label>
-						<div className="relative">
-							<input
-								className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-								id="name"
-								type="name"
-								name="name"
-								placeholder="Enter your name"
 								required
 							/>
 							<AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
@@ -80,12 +78,31 @@ export default function SignupForm() {
 							<KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
 						</div>
 					</div>
+					<div className="mt-4">
+						<label
+							className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+							htmlFor="passwordRe"
+						>
+							Password
+						</label>
+						<div className="relative">
+							<input
+								className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+								id="passwordRe"
+								type="password"
+								name="passwordRe"
+								placeholder="Enter password again"
+								required
+								minLength={6}
+							/>
+							<KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+						</div>
+					</div>
+					<input type="hidden" name="redirectTo" value={callbackUrl} />
+					<Button className="mt-4 w-full" aria-disabled={isPending}>
+						Sign up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
+					</Button>
 				</div>
-				<input type="hidden" name="redirectTo" value={callbackUrl} />
-				<Button className="mt-4 w-full" aria-disabled={isPending}>
-					Sign up <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-				</Button>
-			</div>
 				<div className="flex h-8 items-end space-x-1">
 					{errorMessage && (
 						<>
